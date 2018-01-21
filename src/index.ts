2f73ba87870b682d7ec93f868ee784ea89d8da34
GregRos <life.gregr@gmail.com>
2018-01-21 15:37:01 +0200

    Fixed exported types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /**
  * @module char-info
  */ /** */
-export {StaticCodeInfo, StaticIndicators, CharClassIndicator} from './inner/abstract'
+export {StaticCodeInfo, StaticIndicators, CharClassIndicator, StaticCharInfo} from './inner/abstract'
 export {Indicators, CodeInfo, CharInfo} from './inner/namespaces';
 export {Interval} from 'node-interval-tree';
